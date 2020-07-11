4ddc4cc0f542eabd49912b51389c7405dfc87f7c
AndreiIlie <andrei.ilie98@gmail.com>
2020-07-11 15:20:18 +0300

    Lesson 16

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,11 +10,27 @@
         protected $params = [];
 
         public function __construct() {
-            $this->getUrl();
+            $url = $this->getUrl();
+
+            // Look in controllers for matching name
+            // ucwords - capitalizes the first character of the string
+            if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                $this->currentController = ucwords($url[0]);
+                unset($url[0]);
+            }
+
+            require_once '../app/controllers/' . $this->currentController . '.php';
+
+            $this->currentController = new $this->currentController;
         }
 
         public function getUrl() {
-            echo $_GET['url'];
+            if(isset($_GET['url'])) {
+                $url = rtrim($_GET['url'], '/'); // Trim / characters
+                $url = filter_var($url, FILTER_SANITIZE_URL); // Sanitize the URL
+                $url = explode('/', $url); // Splits $url into its parts
+                return $url;
+            }
         }
     };
 
