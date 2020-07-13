fe870014985122505eb598cc8227e0e0b2fac4cf
AndreiIlie <andrei.ilie98@gmail.com>
2020-07-13 12:01:33 +0300

    Lesson 20 - Config File & Uploader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,10 @@
 <?php
+    require_once 'config/config.php';
+
     // Load Libraries
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';
+
+    spl_autoload_register(function($clsName) {
+        require_once 'libraries/' . $clsName . '.php';
+    });
 
 ?>
---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://localhost/course/');
+define('SITENAME', 'OPCourse')
+
+
+?>
