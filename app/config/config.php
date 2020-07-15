090d7e1d1a197971bc8c4b44cd476c45c6ba213b
AndreiIlie <andrei.ilie98@gmail.com>
2020-07-15 18:03:56 +0300

    Lesson 26 - Initial App & Database Setup

--- app/config/config.php
+++ app/config/config.php
@@ -2,13 +2,13 @@
 
 // General application variables
 define('APPROOT', dirname(dirname(__FILE__)));
-define('URLROOT', ''); // URL to the root of the application (e.g. http://localhost/app)
-define('SITENAME', ''); // Website name, used in page titles and others.
+define('URLROOT', 'http://localhost/course'); // URL to the root of the application (e.g. http://localhost/app)
+define('SITENAME', 'OPApp'); // Website name, used in page titles and others.
 
 // Database connection details
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', '');
+define('DB_PASS', 'frogs');
+define('DB_NAME', 'opappdb');
 
 ?>
